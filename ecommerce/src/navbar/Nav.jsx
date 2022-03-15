253e03e0083f22045e085f99e97c04f3ef21cc2e
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-15 17:43:02 +0530

    feature added navbar

---
+++ ecommerce/src/navbar/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Container= styled.div`
+    height: 60px;
+    background-color: black;
+`
+export const Nav = () => {
+  return (
+    <Container>Navbar</Container>
+  )
+}
---
+++ ecommerce/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Nav } from '../navbar/Nav'
+
+export default function Home() {
+  return (
+    <div>
+        <Nav></Nav>
+    </div>
+  )
+}
