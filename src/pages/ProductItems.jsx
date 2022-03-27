8c47e995fefba1168a04fa896bdde82afe48e73c
Pooja Kelkar <poojakelkar53@gmail.com>
2022-03-27 23:37:02 +0530

    added api and product

--- src/pages/ProductItems.jsx
+++ src/pages/ProductItems.jsx
@@ -11,14 +11,22 @@ import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
+const Grid = styled.div`
+  overflow: hidden;
+  display: grid;
+  height: 130vh;
+  color: white;
+  grid-template-areas: "Sidebar SideContainer SideContainer SideContainer";
+  grid-gap: 10px;
+`;
 const SideBar = styled.div`
-  background-color: red;
+  color: black;
   width: 20vw;
   height: 130vh;
   display: flex;
   flex-direction: column;
   flex-wrap: wrap;
-  margin: 0px 20px;
+  margin: 0px 0px 0px 20px;
   padding: 10px 0px 0px 0px;
 `;
 const Filter = styled.div`
@@ -83,7 +91,55 @@ const Radiobtn = styled.div`
 const Radio = styled.input.attrs({ type: "radio" })``;
 
 const SideContainer = styled.div`
-  background-color: red;
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  background-color: #e9e9e9;
+  border-radius: 10px;
+  padding: 10px;
+`;
+const Card = styled.div`
+  margin: 10px;
+`;
+const ProductImg = styled.div`
+  width: 250px;
+  height: 400px;
+  background-color: white;
+`;
+const Image = styled.img`
+  width: 250px;
+  height: 300px;
+  object-fit: cover;
+  border: 1px solid black;
+`;
+const ImgInfo = styled.div`
+  color: black;
+  padding: 7px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+const ImgTitle = styled.p`
+  font-size: 18px;
+  font-weight: 400;
+`;
+const CategoryName = styled.p`
+  font-size: 22px;
+  font-weight: 600;
+`;
+const Button = styled.button`
+  width: 250px;
+  padding: 10px 20px;
+  background-color: teal;
+  border: none;
+  color: white;
+  cursor: pointer;
+  font-size: 18px;
+  font-weight: 400;
+
+  &:hover {
+    background-color: #66b2b2;
+  }
 `;
 
 export const ProductItems = () => {
@@ -123,66 +179,72 @@ export const ProductItems = () => {
     <Container>
       <Announce></Announce>
       <Nav></Nav>
-      <SideBar>
-        <Filter>
-          <Title>Filter</Title>
-          <Clear>Clear</Clear>
-        </Filter>
-        <PriceSlider>
-          <Title>Price</Title>
-          <SlideContainer>
-            <PriceSliderText>
-              <Price>50</Price>
-              <Price>200</Price>
-              <Price>500</Price>
-            </PriceSliderText>
-            <PriceSlide></PriceSlide>
-          </SlideContainer>
-        </PriceSlider>
+      <Grid>
+        <SideBar>
+          <Filter>
+            <Title>Filter</Title>
+            <Clear>Clear</Clear>
+          </Filter>
+          <PriceSlider>
+            <Title>Price</Title>
+            <SlideContainer>
+              <PriceSliderText>
+                <Price>50</Price>
+                <Price>200</Price>
+                <Price>500</Price>
+              </PriceSliderText>
+              <PriceSlide></PriceSlide>
+            </SlideContainer>
+          </PriceSlider>
 
-        <Category>
-          <Title>Category</Title>
-          {!!categories?.length &&
-            categories.map((category, index) => (
-              <CheckBoxItem key={category._id}>
-                <CheckBox />
-                {category?.categoryName}
-              </CheckBoxItem>
+          <Category>
+            <Title>Category</Title>
+            {!!categories?.length &&
+              categories.map((category, index) => (
+                <CheckBoxItem key={category._id}>
+                  <CheckBox />
+                  {category?.categoryName}
+                </CheckBoxItem>
+              ))}
+          </Category>
+
+          <Radiobtn>
+            <Title>Rating</Title>
+            <RadioContainer>
+              <Radio type="radio" id="star1" value="star1" name="stars" /> 4
+              Star and above
+            </RadioContainer>
+            <RadioContainer>
+              <Radio type="radio" id="star2" value="star2" name="stars" /> 3
+              Star and above
+            </RadioContainer>
+            <RadioContainer>
+              <Radio type="radio" id="star3" value="star3" name="stars" /> 2
+              Star and above
+            </RadioContainer>
+            <RadioContainer>
+              <Radio type="radio" id="star4" value="star4" name="stars" /> 1
+              Star and above
+            </RadioContainer>
+          </Radiobtn>
+        </SideBar>
+
+        <SideContainer>
+          {!!products?.length &&
+            products.map((item) => (
+              <Card>
+                <ProductImg key={item._id}>
+                  <Image src={item.image} />
+                  <ImgInfo>
+                    <ImgTitle>{item?.title}</ImgTitle>
+                    <CategoryName>Rs.{item?.price}</CategoryName>
+                  </ImgInfo>
+                  <Button>ADD TO CARD</Button>
+                </ProductImg>
+              </Card>
             ))}
-        </Category>
-
-        <Radiobtn>
-          <Title>Rating</Title>
-          <RadioContainer>
-            <Radio type="radio" id="star1" value="star1" name="stars" /> 4 Star
-            and above
-          </RadioContainer>
-          <RadioContainer>
-            <Radio type="radio" id="star2" value="star2" name="stars" /> 3 Star
-            and above
-          </RadioContainer>
-          <RadioContainer>
-            <Radio type="radio" id="star3" value="star3" name="stars" /> 2 Star
-            and above
-          </RadioContainer>
-          <RadioContainer>
-            <Radio type="radio" id="star4" value="star4" name="stars" /> 1 Star
-            and above
-          </RadioContainer>
-        </Radiobtn>
-      </SideBar>
-
-      {/* <SideContainer>
-       
-      </SideContainer> */}
-      {!!products?.length &&
-        products.map((item) => (
-          <div key={item._id}>
-            <p>{item?.title}</p>
-            <p>{item?.categoryName}</p>
-            <img src={item.image} />
-          </div>
-        ))}
+        </SideContainer>
+      </Grid>
       <Footer></Footer>
       <LastFooter></LastFooter>
     </Container>
