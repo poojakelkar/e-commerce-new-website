ddffd36265555db46d4fdf74701f53cb3756b6c5
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-05 16:42:45 +0530

    added index file

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+//import { CounterProvider } from "./Context/counterContext";
+import Context from "./Context";
+import { BrowserRouter } from "react-router-dom";
+import { CounterProvider } from "./counterContext";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Context>
+      <CounterProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </CounterProvider>
+    </Context>
   </React.StrictMode>,
   document.getElementById("root")
 );
