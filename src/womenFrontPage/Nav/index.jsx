2226e5de4ee9b158452e2627552c7f5390fb95aa
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-05 12:48:12 +0530

    added annonce and navbar page with style

---
+++ src/womenFrontPage/Nav/index.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import { Badge } from "@material-ui/core";
+
+import {
+  Center,
+  Container,
+  Input,
+  Item,
+  Lang,
+  Left,
+  Logo,
+  NavComponent,
+  Right,
+  SearchContainer,
+  StyledLink,
+} from "./styles";
+
+export const Nav = () => {
+  return (
+    <Container>
+      <NavComponent>
+        <Left>
+          <Lang>EN</Lang>
+          <SearchContainer>
+            <Input placeholder="Search"></Input>
+            <Search style={{ color: "grey", fontSize: 16 }}></Search>
+          </SearchContainer>
+        </Left>
+        <StyledLink to="/">
+          <Center>
+            <Logo>WOMEN.</Logo>
+          </Center>
+        </StyledLink>
+        <Right>
+          <Item>USER</Item>
+          <Item>
+            <StyledLink to="/cart">
+              <Badge badgeContent={2} color="primary">
+                <ShoppingCartOutlined />
+              </Badge>
+            </StyledLink>
+          </Item>
+        </Right>
+      </NavComponent>
+    </Container>
+  );
+};
