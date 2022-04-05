71f0d753684113647147575c1cfa99ae8d08f1a0
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-05 15:21:55 +0530

    addedproduct card slider and its functionality

---
+++ src/Reducer.js
@@ -0,0 +1,57 @@
+const stateReducer = (state, action) => {
+  switch (action.type) {
+    case "SET_PRODUCTS":
+      return { ...state, products: action.payload };
+
+    case "SET_SORT_TYPE":
+      return { ...state, sortBy: action.payload };
+
+    case "SET_CATEGORY":
+      return state.sortByFilters.sortByCategory.includes(action.payload)
+        ? {
+            ...state,
+            sortByFilters: {
+              ...state.sortByFilters,
+              sortByCategory: state.sortByFilters.sortByCategory.filter(
+                (item) => item !== action.payload
+              ),
+            },
+          }
+        : {
+            ...state,
+            sortByFilters: {
+              ...state.sortByFilters,
+              sortByCategory: state.sortByFilters.sortByCategory.concat(
+                action.payload
+              ),
+            },
+          };
+
+    case "SET_RATING":
+      return { ...state, rating: action.payload };
+
+    case "CLEAR_ALL_FILTERS":
+      return {
+        ...state,
+        sortBy: null,
+        priceRange: 0,
+        rating: 0,
+        sortByFilters: {
+          sortByCategory: [],
+          sortByBrands: [],
+        },
+        subCategory: null,
+        tag: null,
+      };
+    case "SET_CART":
+      return { ...state, cart: action.payload };
+    case "SET_WISHLIST":
+      return { ...state, wishlist: action.payload };
+    case "SET_TAG":
+      return { ...state, tag: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default stateReducer;
