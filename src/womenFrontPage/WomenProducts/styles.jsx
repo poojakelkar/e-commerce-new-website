0dce1cac988d29ea99268fc8ee56357e094457e5
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-05 13:47:45 +0530

    added womens product component with styles

---
+++ src/womenFrontPage/WomenProducts/styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  padding: 0rem 1.5rem;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+export const Head = styled.h1`
+  display: flex;
+  margin: 1.5rem 0rem 0rem 1rem;
+  font-size: 1.5rem;
+`;
