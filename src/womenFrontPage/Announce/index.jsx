f92575fa50b2f403de901c90ac76c7ed9b13b9b9
Pooja Kelkar <poojakelkar53@gmail.com>
2022-04-15 13:08:59 +0530

    delete some file

--- src/womenFrontPage/Announce/index.jsx
+++
@@ -1,5 +0,0 @@
-import { Container } from "./styles";
-
-export const Announce = () => {
-  return <Container>Superb Offer! Free shipping over 1000rs.</Container>;
-};
--- src/womenFrontPage/Category/styles.jsx
+++
@@ -1,9 +0,0 @@
-import { mobile } from "../../responsive";
-import styled from "styled-components";
-
-export const Container = styled.div`
-  display: flex;
-  padding: 1rem;
-  justify-content: space-between;
-  ${mobile({ flexDirection: "column", padding: "0rem" })}
-`;
